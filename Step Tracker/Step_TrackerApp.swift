81fd719add2e1c413c9e55e4a8872bdffde71f49
richhendershot <75634077+RichHendershot@users.noreply.github.com>
2026-01-01 19:33:59 -0500

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Richard Hendershot on 1/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
